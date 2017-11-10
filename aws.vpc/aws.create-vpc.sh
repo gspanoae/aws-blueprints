6e9b728724ecaa78457e38148c8bca3278cd9443
Gabi Spanoae <gspanoae@gmail.com>
2017-11-10 11:55:24 +0000

    rename tags

--- aws.vpc/aws.create-vpc.sh
+++ aws.vpc/aws.create-vpc.sh
@@ -3,22 +3,22 @@
 # ###########################################################
 # Set VPC CIDR and tags
 VPC_CIDR=10.1.0.0/16
-VPC_TAG_NAME=sysVpc
+VPC_TAG_NAME=sys-vpc
 # ###########################################################
 # Set VPC Subnets CIDR and tags
 SUBNET_PUBLIC_2a_CIDR=10.1.10.0/24
-SUBNET_PUBLIC_2a_TAG_NAME=sysVpc-net-public2a
+SUBNET_PUBLIC_2a_TAG_NAME=sys-vpc-net-public2a
 SUBNET_PRIVATE_2a_CIDR=10.1.11.0/24
-SUBNET_PRIVATE_2a_TAG_NAME=sysVpc-net-private2a
+SUBNET_PRIVATE_2a_TAG_NAME=sys-vpc-net-private2a
 
 SUBNET_PUBLIC_2b_CIDR=10.1.20.0/24
-SUBNET_PUBLIC_2b_TAG_NAME=sysVpc-net-public2b
+SUBNET_PUBLIC_2b_TAG_NAME=sys-vpc-net-public2b
 SUBNET_PRIVATE_2b_CIDR=10.1.21.0/24
-SUBNET_PRIVATE_2b_TAG_NAME=sysVpc-net-private2b
+SUBNET_PRIVATE_2b_TAG_NAME=sys-vpc-net-private2b
 # ###########################################################
 # Set Route Table tags
-ROUTE_TABLE_PUBLIC_TAG_NAME=sysRtb-public
-ROUTE_TABLE_PRIVATE_TAG_NAME=sysRtb-private
+ROUTE_TABLE_PUBLIC_TAG_NAME=sys-rtb-public
+ROUTE_TABLE_PRIVATE_TAG_NAME=sys-rtb-private
 # ###########################################################
 
 # -----------------------------------------------------------
@@ -30,8 +30,8 @@ SUBNET_PUBLIC_2b_ID=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$VPC
 SUBNET_PRIVATE_2b_ID=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$VPC_ID Name=cidr,Values=$SUBNET_PRIVATE_2b_CIDR --query 'Subnets[].SubnetId' --output text)
 
 # you can't remove the Main=true VPC route table
-# ROUTE_TABLE_PUBLIC_ID=$(aws ec2 describe-route-tables --filters Name=vpc-id,Values=$VPC_ID Name=tag:Name,Values=$ROUTE_TABLE_PUBLIC_TAG_NAME --query 'RouteTables[].RouteTableId' --output text)
 # ROUTE_TABLE_PUBLIC_MAIN_RTA_ID=$(aws ec2 describe-route-tables --filter Name=vpc-id,Values=$VPC_ID --query "RouteTables[].Associations[?Main=='true'].RouteTableAssociationId" --output text)
+# ROUTE_TABLE_PUBLIC_ID=$(aws ec2 describe-route-tables --filters Name=vpc-id,Values=$VPC_ID Name=tag:Name,Values=$ROUTE_TABLE_PUBLIC_TAG_NAME --query 'RouteTables[].RouteTableId' --output text)
 ROUTE_TABLE_PRIVATE_ID=$(aws ec2 describe-route-tables --filters Name=vpc-id,Values=$VPC_ID Name=tag:Name,Values=$ROUTE_TABLE_PRIVATE_TAG_NAME --query 'RouteTables[].RouteTableId' --output text)
 
 SUBNET_PUBLIC_2a_RTA_ID=$(aws ec2 describe-route-tables --filter Name=association.subnet-id,Values=$SUBNET_PUBLIC_2a_ID --query "RouteTables[].Associations[?SubnetId=='$SUBNET_PUBLIC_2a_ID'].RouteTableAssociationId" --output text)
